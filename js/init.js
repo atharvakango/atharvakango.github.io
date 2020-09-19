84edb40b6ce3dd519c110a48cb81b588c9749874
Atharva Kango <atharvakango1@gmail.com>
2020-09-19 19:01:16 +0530

    Minor updates after job offer

--- js/init.js
+++ js/init.js
@@ -512,7 +512,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Software Development Engineer", "a Part Time Freelancer", "a Django Developer"],
+			strings: ["Software Developer", "a Part Time Freelancer", "a Django Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
